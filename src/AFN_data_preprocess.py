4ac77f1df6a87942b40e132007f78a53040a833e
getwellman <1724138936@qq.com>
2023-02-01 10:57:38 +0800

    reloopcode5

--- src/AFN_data_preprocess.py
+++ src/AFN_data_preprocess.py
@@ -20,11 +20,14 @@ import pandas as pd
 from pathlib import Path
 
 
+file_path1 = "../data_mov/AFN_data/"
+file_path2 = "../data_mov/origin_data/"
+if not os.path.exists(file_path1):
+    os.makedirs(file_path1)
+if not os.path.exists(file_path2):
+    os.makedirs(file_path2)
 headers = ["label", "user_id", "item_id", "tag_id"]
 data_files = ["../data_mov/AFN_data/train.libsvm", "../data_mov/AFN_data/valid.libsvm"]
-file_path = "../data_mov/origin_data/"
-if not os.path.exists(file_path):
-    os.makedirs(file_path)
 
 for f in data_files:
     df = pd.read_csv(f, sep=" ", names=headers)
